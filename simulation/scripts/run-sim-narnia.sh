4f222ba1cca1bf0b90595110554803f1ee5e2658
pzapataf <pzapataf@redhat.com>
2018-12-31 19:49:24 +0100

    Update docs

--- simulation/scripts/run-sim-narnia.sh
+++ simulation/scripts/run-sim-narnia.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-POPULATION_SIZE=1000
-MONTHS=1200
+POPULATION_SIZE=50000
+MONTHS=720
 YEAR=2018
 
 ./ows.sh Narnia-0 Narnia.defaults months=${MONTHS} year=${YEAR} output=../simulation/output-sim \
@@ -13,20 +13,19 @@ YEAR=2018
 ./ows.sh Narnia-1 Narnia.defaults months=${MONTHS} year=${YEAR} output=../simulation/output-sim \
     INITIAL_POPULATION_SIZE=$POPULATION_SIZE \
     MIGRATION_INFLOW_BASE_PCT=0.5 \
-    MIGRATION_OUTFLOW_BASE_PCT=1
+    MIGRATION_OUTFLOW_BASE_PCT=0
 
 ./ows.sh Narnia-2 Narnia.defaults months=${MONTHS} year=${YEAR} output=../simulation/output-sim \
     INITIAL_POPULATION_SIZE=$POPULATION_SIZE \
-    MIGRATION_INFLOW_BASE_PCT=1   \
-    MIGRATION_OUTFLOW_BASE_PCT=1
+    MIGRATION_INFLOW_BASE_PCT=0.75   \
+    MIGRATION_OUTFLOW_BASE_PCT=0
 
 ./ows.sh Narnia-1.1 Narnia.defaults months=${MONTHS} year=${YEAR} output=../simulation/output-sim \
     INITIAL_POPULATION_SIZE=$POPULATION_SIZE \
     MIGRATION_INFLOW_BASE_PCT=0.5   \
-    MIGRATION_OUTFLOW_BASE_PCT=1 \
-    EVOLVE_TOTAL_PCT_MATERNITY_NUM_CHILDREN_MEAN=50 \
+    MIGRATION_OUTFLOW_BASE_PCT=0 \
+    EVOLVE_TOTAL_PCT_MATERNITY_NUM_CHILDREN_MEAN=30 \
     EVOLVE_TOTAL_PCT_MATERNITY_AGE_MEAN=-20.0
-
 
 
 #./ows.sh Narnia-3 Narnia.defaults months=1200 year=2017 output=../simulation/output-sim \
